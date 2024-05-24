298fb187b143b1fbcd29ac57cce11592ff557388
Ahmed Maher <62140280+AhmedMaherSayed@users.noreply.github.com>
2024-05-24 17:22:21 +0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -37,4 +37,153 @@ function populateCards(value) {
     }
   }
   
+  const result = document.getElementById("result");
+  const score = document.getElementById("score");
+  var total = 0;
+  const restart = document.getElementById("restart");
+  const reveal = document.getElementById("reveal");
+  const startArea = document.getElementById("startArea");
   
+  function CountDownTimer(duration, granularity) {
+    this.duration = duration;
+    this.granularity = granularity || 1000;
+    this.tickFtns = [];
+    this.running = false;
+  }
+  
+  CountDownTimer.prototype.start = function () {
+    if (this.running) {
+      return;
+    }
+    this.running = true;
+    var start = Date.now(),
+      that = this,
+      diff,
+      obj;
+  
+    (function timer() {
+      diff = that.duration - (((Date.now() - start) / 1000) | 0);
+  
+      if (diff > 0) {
+        setTimeout(timer, that.granularity);
+      } else {
+        diff = 0;
+        that.running = false;
+        blackout();
+      }
+  
+      obj = CountDownTimer.parse(diff);
+      that.tickFtns.forEach(function (ftn) {
+        ftn.call(this, obj.minutes, obj.seconds);
+      }, that);
+    })();
+  };
+  
+  CountDownTimer.prototype.onTick = function (ftn) {
+    if (typeof ftn === "function") {
+      this.tickFtns.push(ftn);
+    }
+    return this;
+  };
+  
+  CountDownTimer.prototype.expired = function () {
+    return !this.running;
+  };
+  
+  CountDownTimer.parse = function (seconds) {
+    return {
+      minutes: (seconds / 60) | 0,
+      seconds: seconds % 60 | 0,
+    };
+  };
+  
+  function blackout() {
+    const x = document.getElementsByClassName("card");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.backgroundColor = "black";
+      x[i].style.pointerEvents = "auto";
+    }
+  }
+  
+  function disablecards() {
+    const x = document.getElementsByClassName("card");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.pointerEvents = "none";
+    }
+  }
+  
+  function revealCards() {
+    const x = document.getElementsByClassName("card");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.backgroundColor = "#ccc";
+    }
+  }
+  
+  window.onload = function () {
+    const display = document.querySelector("#time"),
+      timer = new CountDownTimer(5),
+      timeObj = CountDownTimer.parse(5);
+  
+    format(timeObj.minutes, timeObj.seconds);
+  
+    timer.onTick(format);
+  
+    document.querySelector("button.start").addEventListener("click", function () {
+      timer.start();
+      revealCards();
+      startArea.classList.add("hidden");
+    });
+  
+    function format(minutes, seconds) {
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      display.textContent = minutes + ":" + seconds;
+    }
+  };
+  
+  function startGame() {
+    const countdown = 10,
+      display = document.querySelector("#time");
+    startTimer(countdown, display);
+  }
+  
+  function startTimer(duration, display) {
+    var timer = duration,
+      minutes,
+      seconds;
+    setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+  
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+      display.textContent = minutes + ":" + seconds;
+  
+      if (--timer < 0) {
+        timer = duration;
+      }
+    }, 1000);
+  }
+  
+  function match(clicked_id, numbers, displayNumbers) {
+    if (clicked_id === numbers[0]) {
+      document.getElementById(clicked_id).style.backgroundColor = "#ccc";
+      numbers.shift();
+      total = total + 100 / displayNumbers.length;
+      score.innerHTML = total;
+      if (numbers.length === 0) {
+        disablecards();
+        result.innerHTML = "Winner Winner Chicken Dinner!";
+        result.style.color = "green";
+        restart.classList.remove("hidden");
+      }
+    } else {
+      disablecards();
+      score.innerHTML = total;
+      result.innerHTML = "Game over! Try again";
+      result.style.color = "red";
+      restart.classList.remove("hidden");
+      reveal.classList.remove("hidden");
+    }
+  }
